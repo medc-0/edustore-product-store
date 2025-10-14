91217773f7661733fb3059ff88659527ef8211be
medc-0 <moonisontop76@gmail.com>
2025-10-14 17:44:17 +0200

    add: deleteProduct function fixed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -67,11 +67,10 @@ export const updateProduct = async (req, res) => {
         `;
 
     if (updateProduct.length === 0) {
-      return res.status(404).json
-      ({ 
+      return res.status(404).json({
         success: false,
-        message: "Product not found" 
-    });
+        message: "Product not found",
+      });
     }
 
     res.status(200).json({ success: true, data: updateProduct[0] });
@@ -83,4 +82,24 @@ export const updateProduct = async (req, res) => {
 
 export const deleteProduct = async (req, res) => {
   // DELETE A PRODUCT
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await sql`
+            DELETE FROM products
+            WHERE id=${id}
+            RETURNING *  
+        `;
+    if (deletedProduct.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({ success: true, data: deletedProduct[0] });
+  } catch (error) {
+    console.log("Error in deleteProduct function", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 };
