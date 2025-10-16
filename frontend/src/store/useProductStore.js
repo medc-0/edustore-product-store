913ea2b16394d75d308f0644578475c4ff08d4b9
medc-0 <moonisontop76@gmail.com>
2025-10-16 17:51:22 +0200

    new: folders and files for preparation

---
+++ frontend/src/store/useProductStore.js
@@ -0,0 +1,6 @@
+import { create } from "zustand";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+// base url will be dynamic depending on the environment
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "";
