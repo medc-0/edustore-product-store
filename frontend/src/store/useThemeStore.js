dc4083bb10475266783f8e79f9a5dd9f9df6e862
medc-0 <moonisontop76@gmail.com>
2025-10-17 22:20:13 +0200

    .

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-    theme: localStorage.getItem("preferred-theme") || "coffee",
-    setTheme: (theme) => {
-        localStorage.setItem("preferred-theme", theme);
-        set({ theme });
-    },
+  theme: localStorage.getItem("preferred-theme") || "forest",
+  setTheme: (theme) => {
+    localStorage.setItem("preferred-theme", theme);
+    set({ theme });
+  },
 }));
