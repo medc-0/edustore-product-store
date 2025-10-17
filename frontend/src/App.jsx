4f2c151b4186e1ceed11ee43091a8ca2668c2d5e
medc-0 <moonisontop76@gmail.com>
2025-10-17 22:21:09 +0200

    new: add routing for HomePage and ProductPage components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
-import Navbar from "./components/Navbar.jsx";
-import HomePage from "./pages/HomePage.jsx";
-import ProductPage from "./pages/ProductPage.jsx";
+import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 import { Routes, Route } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { theme } = useThemeStore();
+
   return (
-    <div className="min-h-screen bg-base-200 transition-colors dureation-300">
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
+
+      <Toaster />
     </div>
   );
 }
